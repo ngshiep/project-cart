48540a0d0ecd2e4480d8a017f6e650869ea4c287
ngshiep <58882423+ngshiep@users.noreply.github.com>
2023-08-14 10:50:38 +0000

    Update index.jsx and index.jsx

--- src/views/pages/LayoutHome/index.jsx
+++ src/views/pages/LayoutHome/index.jsx
@@ -6,7 +6,7 @@ import { Toaster } from "react-hot-toast";
 export default function LayoutHome() {
   return (
     <>
-      <div className="fixed top-0 left-0 right-0 h-[58px] flex justify-center items-center mx-auto md:px-5 px-3 lg:px-[100px]">
+      <div className="fixed top-0 left-0 right-0 h-[58px] z-20 shadow-md shadow-orange-100 bg-white flex justify-center items-center mx-auto md:px-5 px-3 lg:px-[100px]">
         <h2 className="font-bold text-2xl text-center">Shopping Cart</h2>
         <CartIcon></CartIcon>
       </div>
