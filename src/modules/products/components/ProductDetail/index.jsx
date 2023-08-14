4ddce609684c1ed699a87d23ded97ca4bfb1a452
hieppnguyen <n.s.hiep@ttdesignco.com>
2023-08-14 17:46:19 +0700

    fix ui

--- src/modules/products/components/ProductDetail/index.jsx
+++ src/modules/products/components/ProductDetail/index.jsx
@@ -3,7 +3,9 @@ import React from "react";
 const ProductDetail = ({ price, quantity, title }) => {
   return (
     <div className="flex flex-col">
-      <div className="w-full h-[250px]  bg-orange-200 rounded-md">img</div>
+      <div className="w-full h-[250px]  bg-orange-200 rounded-md flex justify-center items-center">
+        img
+      </div>
       <h3 className="text-base mt-2 ">{title}</h3>
       <span className="text-orange-600 capitalize">${price}</span>
       <span className="text-sm text-gray-500">
